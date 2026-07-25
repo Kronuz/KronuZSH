08e3fe9b8e3953262884e88c5767e0fc1ed1f3a2
Germán Méndez Bravo <german.mb@gmail.com>
2026-07-24 18:57:11 -0700

    Fix Spaceship skin second prompt line

--- skins/spaceship.zsh
+++ skins/spaceship.zsh
@@ -1,5 +1,5 @@
 # spaceship — Spaceship's familiar two-line directory/context/Git layout.
-KZ_PROMPT_GIT='${kz[git.branch]:+${kz[FG.blue]}on ${kz[git.branch]}${kz[git.dirty]:+ ${kz[FG.red]}✗}${kz[git.staged]:+ ${kz[FG.green]}✚}${kz[git.ahead]:+ ⇡}${kz[git.behind]:+ ⇣}${kz[RESET]}}'
-KZ_PROMPT_PROMPT='${kz[FG.cyan]}➜ ${kz[FG.green]}%~${kz[RESET]}$kz[git]${kz[venv.name]:+ ${kz[FG.yellow]}via ${kz[venv.name]}${kz[RESET]}}$kz[nl]'
+KZ_PROMPT_GIT='${kz[git.branch]:+ ${kz[FG.blue]}on ${kz[git.branch]}${kz[git.dirty]:+ ${kz[FG.red]}✗}${kz[git.staged]:+ ${kz[FG.green]}✚}${kz[git.ahead]:+ ⇡}${kz[git.behind]:+ ⇣}${kz[RESET]}}'
+KZ_PROMPT_PROMPT='${kz[FG.cyan]}➜ ${kz[FG.green]}%~${kz[RESET]}$kz[git]${kz[venv.name]:+ ${kz[FG.yellow]}via ${kz[venv.name]}${kz[RESET]}}$kz[nl]%(?.${kz[FG.green]}.%F{red})❯ '
 KZ_PROMPT_RPROMPT=''
 KZ_PROMPT_TRANSIENT_PROMPT='${kz[FG.green]}%~${kz[RESET]} %(?.${kz[FG.green]}.%F{red})❯ '
